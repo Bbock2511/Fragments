801824303be20b9d7e6d2d35f23b752cceb01ea4
Bbock2511 <128552505+Bbock2511@users.noreply.github.com>
2023-09-21 11:39:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fragments/Fragment2.kt
@@ -0,0 +1,24 @@
+package com.example.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class Fragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_2, container, false)
+
+        view.findViewById<Button>(R.id.buttonSave).setOnClickListener{ findNavController().navigate(R.id.action_fragment2_to_blankFragment1) }
+
+        return view
+    }
+}
